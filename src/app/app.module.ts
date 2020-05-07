373bedc007668bae2da2f6c37b1e3751b01fce1b
samara <hammettdepressivo@gmail.com>
2020-05-06 22:42:59 -0300

    Exercicios 1 a 6

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavComponent } from './nav/nav.component';
+import { UserComponent } from './user/user.component';
+import { PostComponent } from './post/post.component';
+import { CommentComponent } from './comment/comment.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavComponent,
+    UserComponent,
+    PostComponent,
+    CommentComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
